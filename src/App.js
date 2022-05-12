91f2f14ddf96d029d20fecad3632d4742f18018d
Kent Ferolino <kentfero@gmail.com>
2022-05-12 17:03:00 +0800

    Test 2

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <p style={{fontSize: 14}}>
+        <p style={{fontSize: 16}}>
           1. Add an input number and submit button.
           The input number should be added/subtracted to the counter if submit is clicked.
+        </p>
+        <p style={{fontSize: 16}}>
+          2. Add a form with input first name and last name. And a submit button.
+          Once submit is clicked, should show an alert with percentage.
+          Percentage should be 25% after 1 second, 50% after another 1 second, 75% after another 1 second, and 100% after another 1 second.
         </p>
       </header>
     </div>
